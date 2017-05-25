310673409f04024c2ec58825205500fd9d71185c
Travis Tomsu <ttomsu@google.com>
2017-05-25 14:29:25 -0400

    fix(js): Smooth scroll is nice, direct header linking is nicer.

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -32,9 +32,6 @@ $(document).ready(function(){
     $(".author__urls-wrapper button").toggleClass("open");
   });
 
-  // init smooth scroll
-  $("a").smoothScroll({offset: -20});
-
   // add lightbox class to all image links
   $("a[href$='.jpg'],a[href$='.jpeg'],a[href$='.JPG'],a[href$='.png'],a[href$='.gif']").addClass("image-popup");
 
